ea0141cc87df92a7d8f81bfa0d0e02ff12297fb6
haruki7049 <tontonkirikiri@gmail.com>
2023-09-21 13:35:25 +0900

    redefined Token

--- src/data/tokens/mod.rs
+++ src/data/tokens/mod.rs
@@ -1,19 +1,18 @@
 /// Token, which is tokenized value. tokenize function is in lexer module.
+/// this has some element as under.
+/// - LParen
+/// - RParen
+/// - Integer
+/// - Float
+/// - String
 #[derive(Debug, PartialEq)]
 pub enum Token {
-    Number(TokenNumber),
-    Symbol(ReservedWord),
-    String(String),
-    StringQuotation,
     LParen,
     RParen,
-}
-
-/// TokenNumber, which is tokenized number.
-#[derive(Debug, PartialEq)]
-pub enum TokenNumber {
+    StringQuotation,
     Integer(isize),
     Float(f64),
+    String(String),
 }
 
 #[derive(Debug)]
@@ -21,54 +20,16 @@ pub struct TokenError {
     char: char,
 }
 
-/// ReservedWord, contains some words
-#[derive(Debug, PartialEq)]
-pub enum ReservedWord {
-    Define,
-    If,
-    Lambda,
-    Operation(Operation),
-    Comparison(Comparison),
-}
-
-/// Operation, contains some words which is used by ReservedWord enum.
-#[derive(Debug, PartialEq)]
-pub enum Operation {
-    Plus,
-    Minus,
-    Asterisk,
-    Slash,
-}
-
-/// Comparison, contains some words which is used by ReservedWord enum.
-#[derive(Debug, PartialEq)]
-enum Comparison {
-    Shorter,
-    Greater,
-    Equal,
-    NotEqual,
-}
-
 /// Display implementation of Token.
 impl std::fmt::Display for Token {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         match self {
-            Token::Number(token_number) => write!(f, "{}", token_number),
-            Token::String(string) => write!(f, "{}", string),
-            Token::Symbol(string) => write!(f, "{}", string),
-            Token::StringQuotation => write!(f, "\""),
             Token::LParen => write!(f, "("),
             Token::RParen => write!(f, ")"),
-        }
-    }
-}
-
-/// Display implementation for TokenNumber.
-impl std::fmt::Display for TokenNumber {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        match self {
-            TokenNumber::Integer(integer) => write!(f, "{}", integer),
-            TokenNumber::Float(float) => write!(f, "{}", float),
+            Token::StringQuotation => write!(f, "\""),
+            Token::Integer(integer) => write!(f, "{}", integer),
+            Token::Float(float) => write!(f, "{}", float),
+            Token::String(string) => write!(f, "{}", string),
         }
     }
 }
@@ -77,41 +38,5 @@ impl std::fmt::Display for TokenNumber {
 impl std::fmt::Display for TokenError {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         write!(f, "unexpected character: {}", self.char)
-    }
-}
-
-/// Display implementation for ReservedWord.
-impl std::fmt::Display for ReservedWord {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        match self {
-            ReservedWord::Define => write!(f, "Define"),
-            ReservedWord::If => write!(f, "If"),
-            ReservedWord::Lambda => write!(f, "Lambda"),
-            ReservedWord::Operation(operation) => write!(f, "{}", operation),
-            ReservedWord::Comparison(comparison) => write!(f, "{}", comparison),
-        }
-    }
-}
-
-/// Display implementation for Operation.
-impl std::fmt::Display for Operation {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        match self {
-            Operation::Plus => write!(f, "Plus"),
-            Operation::Minus => write!(f, "Minus"),
-            Operation::Asterisk => write!(f, "Asterisk"),
-            Operation::Slash => write!(f, "Slash"),
-        }
-    }
-}
-
-impl std::fmt::Display for Comparison {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        match self {
-            Comparison::Shorter => write!(f, "Shorter"),
-            Comparison::Greater => write!(f, "Greater"),
-            Comparison::Equal => write!(f, "Equal"),
-            Comparison::NotEqual => write!(f, "NotEqual"),
-        }
     }
 }
