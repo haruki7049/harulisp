1fd236540b232b4385b511eb8316bbfa17e03f45
haruki7049 <tontonkirikiri@gmail.com>
2023-10-13 20:24:10 +0900

    hoge

--- src/data/env/mod.rs
+++ src/data/env/mod.rs
@@ -1,4 +1,24 @@
-use crate::data::objects::Object;
+use crate::data::{
+    objects::{
+        Object,
+        Symbol,
+        Operator,
+        Comparison,
+        DEFINE,
+        IF,
+        LAMBDA,
+
+        PLUS,
+        MINUS,
+        ASTERISK,
+        SLASH,
+
+        SHORTER,
+        GREATER,
+        EQUAL,
+        NOT_EQUAL,
+    },
+};
 use std::cell::RefCell;
 use std::collections::HashMap;
 use std::rc::Rc;
@@ -6,7 +26,7 @@ use std::rc::Rc;
 #[derive(Debug, PartialEq, Default)]
 pub struct Env {
     parent: Option<Rc<RefCell<Env>>>,
-    vars: HashMap<String, Object>,
+    variables: HashMap<String, Object>,
 }
 
 impl Env {
@@ -16,17 +36,42 @@ impl Env {
 
     pub fn extend(parent: Rc<RefCell<Self>>) -> Env {
         Env {
-            vars: HashMap::new(),
+            variables: HashMap::new(),
             parent: Some(parent),
         }
     }
 
     pub fn get(&self, name: &str) -> Option<Object> {
-        match self.vars.get(name) {
+        match self.variables.get(name) {
             Some(value) => Some(value.clone()),
             None => self.parent
                 .as_ref()
                 .and_then(|o| o.borrow().get(name).clone()),
+        }
+    }
+    
+    pub fn set(&mut self, name: &str, value: Object) {
+        self.variables.insert(name.to_string(), value);
+    }
+}
+
+/// get_string_from_symbol function which is convert Symbol to String
+pub fn convert_string_from_symbol(symbol: &Symbol) -> String {
+    match symbol {
+        Symbol::Define => String::from(DEFINE),
+        Symbol::If => String::from(IF),
+        Symbol::Lambda => String::from(LAMBDA),
+        Symbol::Operator(operator) => match operator {
+            Operator::Plus => String::from(PLUS),
+            Operator::Minus => String::from(MINUS),
+            Operator::Asterisk => String::from(ASTERISK),
+            Operator::Slash => String::from(SLASH),
+        }
+        Symbol::Comparison(comparison) => match comparison {
+            Comparison::Shorter => String::from(SHORTER),
+            Comparison::Greater => String::from(GREATER),
+            Comparison::Equal => String::from(EQUAL),
+            Comparison::NotEqual => String::from(NOT_EQUAL),
         }
     }
 }
