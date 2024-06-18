1e078fbbc0f735915e6601ae021c48434be8d498
haruki7049 <64677724+haruki7049@users.noreply.github.com>
2024-06-19 05:07:24 +0900

    feat!: Setup this repo for v0.2.0 (#30)
    * feat: cargo-init
    
    * feat: Init nix-flakes

--- src/data.rs
+++
@@ -1,8 +0,0 @@
-/// contains Env structure.
-pub mod env;
-
-/// contains Object, used to express harulisp's data structure to Rust structure by parser and evaluator.
-pub mod objects;
-
-/// contains Token, used to express harulisp's token by lexer and parser.
-pub mod tokens;
