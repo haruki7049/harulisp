4b197d0dfb651a837244586b5cc98fe8c3eaef04
haruki7049 <tontonkirikiri@gmail.com>
2025-05-10 12:24:38 +0900

    chore(main.rs): Init clap & anyhow

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+
+fn main() -> anyhow::Result<()> {
+    let args: CLIArgs = CLIArgs::parse();
+    dbg!(args.script);
+
+    Ok(())
+}
+
+#[derive(Parser)]
+#[command(version, about)]
+struct CLIArgs {
+    #[arg(short, long)]
+    script: String,
 }
