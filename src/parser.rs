fb3c9f3ddaaf3b105c59c2eb94e345c30852fefc
haruki7049 <tontonkirikiri@gmail.com>
2025-05-11 21:52:25 +0900

    WIP

--- src/parser.rs
+++ src/parser.rs
@@ -1,16 +1,34 @@
 use pest::Parser;
-use pest::iterators::Pair;
+use pest::iterators::{Pair, Pairs};
 use pest_derive::Parser;
 
 #[derive(Parser)]
 #[grammar = "parser/harulisp.pest"]
 pub struct HarulispParser;
 
-#[derive(Debug)]
-pub struct SExpression;
+#[derive(Debug, Default)]
+pub struct SExpression {
+    function_word: String,
+    arguments: Vec<String>,
+}
 
-fn parse_pair(_pair: Pair<Rule>) -> anyhow::Result<SExpression> {
-    todo!();
+fn parse_pair(pair: Pair<Rule>) -> anyhow::Result<SExpression> {
+    match pair.as_rule() {
+        Rule::EOI | Rule::word | Rule::PUNCT_CHARACTER => unreachable!(),
+        Rule::SExpression => {
+            dbg!(pair.clone().tokens());
+
+            let mut rule = pair.into_inner();
+            let function_word = rule.next().unwrap();
+
+            let arguments: Vec<String> = vec![];
+
+            Ok(SExpression {
+                function_word: String::from(function_word.as_span().as_str()),
+                arguments,
+            })
+        }
+    }
 }
 
 pub fn parse(s: &str) -> anyhow::Result<SExpression> {
