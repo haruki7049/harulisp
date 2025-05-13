b153d9402dcc9fc50f2005a1f636c64427dca29a
haruki7049 <tontonkirikiri@gmail.com>
2025-05-13 16:53:44 +0900

    WIP

--- src/eval.rs
+++ src/eval.rs
@@ -1,8 +1,8 @@
 use crate::parser;
-use crate::parser::SExpression;
+use crate::parser::Program;
 
 pub fn eval(str: String) -> anyhow::Result<()> {
-    let sexpr: SExpression = match parser::parse(str.as_str()) {
+    let sexpr: Program = match parser::parse(str.as_str()) {
         Ok(v) => v,
         Err(e) => anyhow::bail!(e),
     };
--- src/parser.rs
+++ src/parser.rs
@@ -1,38 +1,83 @@
 use pest::Parser;
-use pest::iterators::{Pair, Pairs};
+use pest::iterators::Pair;
 use pest_derive::Parser;
 
 #[derive(Parser)]
 #[grammar = "parser/harulisp.pest"]
 pub struct HarulispParser;
 
-#[derive(Debug, Default)]
-pub struct SExpression {
-    function_word: String,
-    arguments: Vec<String>,
+#[derive(Debug)]
+pub struct Program;
+
+#[derive(Debug)]
+pub enum Token {
+    Word(String),
+    Int(i32),
+    SExpression(Vec<Token>),
 }
 
-fn parse_pair(pair: Pair<Rule>) -> anyhow::Result<SExpression> {
+fn parse_pair(pair: Pair<Rule>) -> anyhow::Result<Program> {
     match pair.as_rule() {
-        Rule::EOI | Rule::word | Rule::PUNCT_CHARACTER => unreachable!(),
-        Rule::SExpression => {
-            dbg!(pair.clone().tokens());
-
+        Rule::EOI
+        | Rule::punct_word
+        | Rule::SExpression
+        | Rule::left_parenthesis
+        | Rule::right_parenthesis
+        | Rule::word
+        | Rule::int => unreachable!(),
+        Rule::program => {
             let mut rule = pair.into_inner();
-            let function_word = rule.next().unwrap();
+            let sexpr: Pair<Rule> = rule.next().unwrap();
 
-            let arguments: Vec<String> = vec![];
-
-            Ok(SExpression {
-                function_word: String::from(function_word.as_span().as_str()),
-                arguments,
-            })
+            match sexpr.as_rule() {
+                Rule::SExpression => {
+                    let result = parse_sexp(sexpr)?;
+                    dbg!(result);
+                    todo!()
+                }
+                v => {
+                    dbg!(v);
+                    todo!()
+                }
+            }
         }
     }
 }
 
-pub fn parse(s: &str) -> anyhow::Result<SExpression> {
-    let mut pairs = HarulispParser::parse(Rule::SExpression, s)?;
+fn parse_sexp(sexpr: Pair<Rule>) -> anyhow::Result<Token> {
+    let result: Token = Token::SExpression(Vec::new());
+
+    let mut rule = sexpr.into_inner();
+    let _left_parenthesis: Pair<Rule> = rule.next().unwrap(); // "("
+    let mut words: Vec<Pair<Rule>> = rule.into_iter().collect(); // "defvar"
+    let _right_parenthesis: Pair<Rule> = words.pop().unwrap(); // ")"
+
+    dbg!(&words);
+
+    for w in words {
+        dbg!(&w);
+
+        match w.as_rule() {
+            Rule::SExpression => {
+                todo!()
+            }
+            Rule::word | Rule::int => {
+                todo!();
+            }
+
+            Rule::EOI
+            | Rule::program
+            | Rule::punct_word
+            | Rule::left_parenthesis
+            | Rule::right_parenthesis => todo!(),
+        };
+    }
+
+    Ok(result)
+}
+
+pub fn parse(s: &str) -> anyhow::Result<Program> {
+    let mut pairs = HarulispParser::parse(Rule::program, s)?;
 
     parse_pair(pairs.next().unwrap())
 }
