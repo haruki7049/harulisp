52929c1f70a077d143761be49b86340649ec0b6e
haruki7049 <tontonkirikiri@gmail.com>
2025-05-14 11:53:04 +0900

    test(parser.rs): Add tests module

--- src/parser.rs
+++ src/parser.rs
@@ -6,12 +6,12 @@ use pest_derive::Parser;
 #[grammar = "parser/harulisp.pest"]
 pub struct HarulispParser;
 
-#[derive(Debug, Default)]
+#[derive(Debug, Default, PartialEq, Eq)]
 pub struct Program {
     statements: Vec<Token>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq)]
 pub enum Token {
     String(String),
     Word(String),
@@ -132,4 +132,37 @@ pub fn parse(s: &str) -> anyhow::Result<Program> {
     let mut pairs = HarulispParser::parse(Rule::program, s)?;
 
     parse_pair(pairs.next().unwrap())
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::parser;
+    use crate::parser::{Program, Token};
+
+    #[test]
+    fn parse() -> anyhow::Result<()> {
+        let result =
+            parser::parse("(def main (lambda '() (println \"This is the example text. FOO!!\")))")?;
+
+        assert_eq!(
+            result,
+            Program {
+                statements: vec![
+                    Token::SExpression(vec![
+                        Token::Word(String::from("def")),
+                        Token::Word(String::from("main")),
+                        Token::SExpression(vec![
+                            Token::Word(String::from("lambda")),
+                            Token::List(vec![]),
+                            Token::SExpression(vec![
+                                Token::Word(String::from("println")),
+                                Token::String(String::from("\"This is the example text. FOO!!\"")),
+                            ])
+                        ])
+                    ])
+                ]
+            }
+        );
+        Ok(())
+    }
 }
