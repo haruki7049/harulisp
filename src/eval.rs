2d55b6b28cf767e0a42e4ebe7519085c65a1b542
haruki7049 <tontonkirikiri@gmail.com>
2025-05-15 13:45:08 +0900

    WIP

--- src/eval.rs
+++ src/eval.rs
@@ -1,9 +1,9 @@
 mod value;
-use value::{Action, Value};
-
 use crate::parser;
 use crate::parser::Program;
+use anyhow::Context as _;
 use std::collections::HashMap;
+use value::{Action, Atom, Type};
 
 /// Evaluates a String as Harulisp
 pub fn eval(str: String) -> anyhow::Result<()> {
@@ -19,14 +19,18 @@ pub fn eval(str: String) -> anyhow::Result<()> {
 
 #[derive(Debug)]
 struct HarulispMachine {
-    variables: HashMap<String, Value>,
+    variables: HashMap<String, Type>,
     entrypoint: String,
 }
 
 impl Machine for HarulispMachine {
     /// Run Harulisp
     fn run(&self) -> anyhow::Result<()> {
-        Ok(())
+        let entrypoint: &Type = self.get(self.entrypoint())?;
+
+        dbg!(entrypoint);
+
+        todo!()
     }
 
     /// Load scripts
@@ -36,17 +40,40 @@ impl Machine for HarulispMachine {
             entrypoint: String::from("main"),
         };
 
+        //machine.append(
+        //    String::from("main"),
+        //    Type::IO(Box::new(Action::Print(Atom::Int(3)))),
+        //);
         machine.append(
             String::from("main"),
-            Value::IO(Box::new(Action::Print(Value::Int(3)))),
+            Type::IO(Box::new(Action::Progn(vec![
+                Action::Print(Atom::Int(1)),
+                Action::Print(Atom::Int(33)),
+            ]))),
         );
+        //machine.append(
+        //    String::from("main"),
+        //    Atom::Int(1),
+        //);
 
         Ok(machine)
     }
 
     /// Append variables
-    fn append(&mut self, name: String, value: Value) {
-        let _ = &self.variables.insert(name, value);
+    fn append(&mut self, n: String, t: Type) {
+        let _ = &self.variables.insert(n, t);
+    }
+
+    fn get(&self, name: &String) -> anyhow::Result<&Type> {
+        Ok(self
+            .variables
+            .get(name)
+            .context(format!("EVAL ERROR: No value found on {}", name))?)
+    }
+
+    /// Reads the entrypoint's name
+    fn entrypoint(&self) -> &String {
+        &self.entrypoint
     }
 }
 
@@ -58,5 +85,11 @@ trait Machine {
     fn load(program: Program) -> anyhow::Result<HarulispMachine>;
 
     /// Append variables
-    fn append(&mut self, name: String, value: Value);
+    fn append(&mut self, n: String, t: Type);
+
+    /// Gets a variable from name: String
+    fn get(&self, name: &String) -> anyhow::Result<&Type>;
+
+    /// Reads the entrypoint's name
+    fn entrypoint(&self) -> &String;
 }
