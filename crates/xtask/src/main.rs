a2edf76f91ecec430426263d30c431bf984d502b
haruki7049 <tontonkirikiri@gmail.com>
2026-04-28 15:32:28 +0900

    build(harulisp_xtask): Create

---
+++ crates/xtask/src/main.rs
@@ -0,0 +1,25 @@
+use clap::Parser;
+use harulisp_xtask::builder::{Builder, SpaceroboBuilder};
+use harulisp_xtask::cli::CLIArgs;
+use std::sync::LazyLock;
+
+type Result = std::result::Result<(), Box<dyn std::error::Error>>;
+
+fn main() -> Result {
+    tracing_subscriber::fmt::init();
+
+    tracing::debug!("Parsing CLI arguments...");
+    let args = CLIArgs::parse();
+    tracing::debug!("Parsed CLI arguments.");
+
+    tracing::debug!("Creating a SpaceroboBuilder...");
+    let builder = SpaceroboBuilder::new(args, CARGO.to_string());
+    tracing::debug!("Created a SpaceroboBuilder.");
+
+    builder.run()?;
+
+    Ok(())
+}
+
+static CARGO: LazyLock<String> =
+    LazyLock::new(|| std::env::var("CARGO").unwrap_or_else(|_| "cargo".to_string()));
